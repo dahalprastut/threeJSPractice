c6edbe3e815bbd0decc6d0b597bfdb09746e5176
dahalprastut <dahalprastut@gmail.com>
2024-04-08 15:48:54 -0400

    Lesson 6 completed

--- script.js
+++ script.js
@@ -1,129 +1,217 @@
-import * as THREE from "three";
-import gsap from "gsap";
+// import * as THREE from "three";
+// import gsap from "gsap";
 
-const canvas = document.querySelector("canvas.webgl");
-// // we need 4 things to create something:
-// // 1. Scene
+// const canvas = document.querySelector("canvas.webgl");
+// // // we need 4 things to create something:
+// // // 1. Scene
+// // const scene = new THREE.Scene();
+
+// // // 2. Object
+// // const geometry = new THREE.BoxGeometry(1, 1, 1);
+// // const material = new THREE.MeshBasicMaterial({
+// // 	color: "red",
+// // });
+// // const mesh = new THREE.Mesh(geometry, material);
+// // // mesh.position.z = -5;
+// // mesh.position.x = 4;
+// // mesh.position.y = 3;
+// // // mesh.position.normalize();
+
+// // // scale
+// // mesh.scale.z = 2;
+// // console.log(mesh.position.length()); // mesh distance from the center of the screen
+// // // Rotation
+// // mesh.rotation.reorder("YXZ");
+// // mesh.rotation.x = 0.5;
+// // mesh.rotation.y = 0.5;
+
+// // scene.add(mesh);
+
+// // // 3. Camera
+// // const sizes = {
+// // 	width: 800,
+// // 	height: 600,
+// // };
+// // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// // camera.position.z = 5;
+// // // camera.position.x = 5;
+// // // camera.position.y = 2;
+// // scene.add(camera);
+// // console.log(mesh.position.distanceTo(camera.position));
+// // camera.lookAt(new THREE.Vector3(0, 0, 0));
+// // // camera.lookAt(mesh.position);
+
+// // const positionHelper = new THREE.AxesHelper(1);
+// // scene.add(positionHelper);
+
+// // // 4. Renderer
+// // const renderer = new THREE.WebGLRenderer({
+// // 	canvas: canvas,
+// // });
+// // renderer.setSize(sizes.width, sizes.height);
+// // renderer.render(scene, camera);
+
+// // Grouping
+
 // const scene = new THREE.Scene();
 
-// // 2. Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({
-// 	color: "red",
-// });
-// const mesh = new THREE.Mesh(geometry, material);
-// // mesh.position.z = -5;
-// mesh.position.x = 4;
-// mesh.position.y = 3;
-// // mesh.position.normalize();
+// const group = new THREE.Group();
+// scene.add(group);
 
-// // scale
-// mesh.scale.z = 2;
-// console.log(mesh.position.length()); // mesh distance from the center of the screen
-// // Rotation
-// mesh.rotation.reorder("YXZ");
-// mesh.rotation.x = 0.5;
-// mesh.rotation.y = 0.5;
+// const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "red" }));
+// cube1.position.x = -2;
+// group.add(cube1);
 
-// scene.add(mesh);
+// const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "blue" }));
+// // cube2.position.x = 2;
+// // cube2.position.y = 2;
+// // cube2.rotation.reorder("YXZ");
+// // cube2.rotation.x = 1;
 
-// // 3. Camera
+// group.add(cube2);
+
+// // group.position.y = 2;
+
 // const sizes = {
 // 	width: 800,
 // 	height: 600,
 // };
 // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 // camera.position.z = 5;
-// // camera.position.x = 5;
+// // camera.position.x = 2;
 // // camera.position.y = 2;
+// // camera.lookAt(new THREE.Vector3(0, 0, 0));
+// // camera.lookAt(cube2.position);
+
 // scene.add(camera);
-// console.log(mesh.position.distanceTo(camera.position));
-// camera.lookAt(new THREE.Vector3(0, 0, 0));
-// // camera.lookAt(mesh.position);
 
-// const positionHelper = new THREE.AxesHelper(1);
-// scene.add(positionHelper);
-
-// // 4. Renderer
 // const renderer = new THREE.WebGLRenderer({
 // 	canvas: canvas,
 // });
 // renderer.setSize(sizes.width, sizes.height);
 // renderer.render(scene, camera);
 
-// Grouping
+// // let time = Date.now();
 
-const scene = new THREE.Scene();
+// // const tick = () => {
+// // 	console.log("tick");
+// // 	const currentTime = Date.now();
+// // 	const deltaTime = currentTime - time;
+// // 	time = currentTime;
+// // 	cube2.position.x += 0.001 * deltaTime;
+// // 	renderer.render(scene, camera);
+// // 	window.requestAnimationFrame(tick);
+// // };
 
-const group = new THREE.Group();
-scene.add(group);
+// const clock = new THREE.Clock();
 
-const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "red" }));
-cube1.position.x = -2;
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "blue" }));
-// cube2.position.x = 2;
-// cube2.position.y = 2;
-// cube2.rotation.reorder("YXZ");
-// cube2.rotation.x = 1;
-
-group.add(cube2);
-
-// group.position.y = 2;
-
-const sizes = {
-	width: 800,
-	height: 600,
-};
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
-// camera.position.x = 2;
-// camera.position.y = 2;
-// camera.lookAt(new THREE.Vector3(0, 0, 0));
-// camera.lookAt(cube2.position);
-
-scene.add(camera);
-
-const renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
-
-// let time = Date.now();
+// // camera.position.y = 4;
+// // camera.lookAt(new THREE.Vector3(0, 2, 0));
+// gsap.to(cube2.position, {
+// 	duration: 2,
+// 	delay: 1,
+// 	x: 3,
+// });
+// gsap.to(cube2.position, {
+// 	duration: 2,
+// 	delay: 2,
+// 	x: 0,
+// });
 
 // const tick = () => {
-// 	console.log("tick");
-// 	const currentTime = Date.now();
-// 	const deltaTime = currentTime - time;
-// 	time = currentTime;
-// 	cube2.position.x += 0.001 * deltaTime;
+// 	// const elapsedTime = clock.getElapsedTime();
+// 	// console.log("ela", elapsedTime);
+// 	// cube2.position.y = Math.sin(elapsedTime);
+// 	// cube2.position.x = Math.cos(elapsedTime);
+// 	// camera.position.x = Math.cos(elapsedTime);
 // 	renderer.render(scene, camera);
 // 	window.requestAnimationFrame(tick);
 // };
 
-const clock = new THREE.Clock();
+// tick();
 
-// camera.position.y = 4;
-// camera.lookAt(new THREE.Vector3(0, 2, 0));
-gsap.to(cube2.position, {
-	duration: 2,
-	delay: 1,
-	x: 3,
-});
-gsap.to(cube2.position, {
-	duration: 2,
-	delay: 2,
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const canvas = document.querySelector("canvas.webgl");
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const cursor = {
 	x: 0,
+	y: 0,
+};
+
+const sizes = {
+	width,
+	height,
+};
+
+window.addEventListener("mousemove", (e) => {
+	cursor.x = e.clientX / sizes.width - 0.5;
+	cursor.y = -(e.clientY / sizes.height - 0.5);
 });
 
+const scene = new THREE.Scene();
+
+const box1 = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshBasicMaterial({
+		color: "red",
+	})
+);
+
+scene.add(box1);
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+
+// orthographic camera
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100);
+
+camera.position.z = 5;
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.update();
+// camera.position.y = 1;
+// camera.lookAt(box1.position);
+console.log(camera.position.length());
+
+scene.add(camera);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+const clock = new THREE.Clock();
 const tick = () => {
-	// const elapsedTime = clock.getElapsedTime();
-	// console.log("ela", elapsedTime);
-	// cube2.position.y = Math.sin(elapsedTime);
-	// cube2.position.x = Math.cos(elapsedTime);
-	// camera.position.x = Math.cos(elapsedTime);
+	const elapsedTime = clock.getElapsedTime();
+	controls.update();
+	// box1.rotation.y = elapsedTime;
+
+	// camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+	// camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+	// camera.position.y = cursor.y * Math.PI * 2;
+	// console.log("m", Math.sin(elapsedTime));
+	// box1.position.x = Math.cos(elapsedTime) * 10;
+	// box1.position.y = Math.sin(elapsedTime) * 10;
+	// camera.position.x = box1.position.x;
+	// camera.position.y += 0.01 * Math.PI;
+	// camera.position.z = Math.cos(elapsedTime);
+	// console.log("camera", camera.position.length());
+	// camera.position.x = Math.sin(elapsedTime) * 4;
+	// camera.position.z = Math.cos(elapsedTime) * 4;
+	// camera.position.y = 5;
+	// camera.lookAt(box1.position);
+	// console.log(camera.position.distanceTo(box1.position));
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(tick);
 };
